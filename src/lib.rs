95b7aa1b90a1cd5bf58033b0b93f9b566fe847ea
ChousX <gmanenergy@gmail.com>
2024-07-14 13:54:36 -0400

    ?

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,104 @@
-pub mod prelude {
-  pub use bevy::prelude::*;
-  pub use bevy::palettes::css as css;
-  pub use css::GOLD;
+pub use bevy::prelude::*;
+use bevy::{render::camera::RenderTarget, window::WindowRef};
+
+pub struct PopOutPlugin;
+impl Plugin for PopOutPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_event::<PopOutEvent>()
+            .add_event::<DespawnWindowEvent>()
+            .add_systems(Update, (
+                    spawn_pop_out_window.run_if(on_event::<PopOutEvent>()), 
+                    despawn_pop_out_window.run_if(on_event::<DespawnWindowEvent>())
+                ));
+    }
 }
 
-pub mod plugins {
-
+#[derive(Event)]
+pub struct PopOutEvent{
+    pub add: Entity,
+    pub pos: Transform,
+    pub title: String,
 }
 
-mod app {
-  use crate::prelude::*;
-  #[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
-  pub enum AppState {
-    #[default]
-    Entry,
-  }
+impl PopOutEvent{
+    pub fn new(add: Entity){
+        Self{
+            add,
+            pos: Transform::default(),
+            title: "".into(),
+        }
+    }
 
-  pub const APP_NAME: &str = "PopOutWindow";
+    pub fn with_pos(mut self, pos: Transform) -> Self{
+        self.pos = pos;
+        self
+    }
+
+    pub fn with_title(mut self, title: impl Into<String>) -> Self {
+        self.title = title.into();
+        self
+    }
+}
+
+#[derive(Component)]
+struct PopOutWindow;
+
+#[derive(Component)]
+struct PopOutCamera;
+
+fn spawn_pop_out_window(
+    mut commands: Commands,
+    mut events: EventReader<PopOutEvent>,
+){
+    for event in events.read(){
+        let PopOutEvent { add, pos, title } = event;
+        let window_id = commands
+            .spawn((
+                Window {
+                    title,
+                    ..default()
+                },
+                PopOutCamera,
+            )).id();
+
+        let camera_id = commands
+            .spawn((
+                Camera2dBundle {
+                    transform: pos,
+                    camera: Camera {
+                        target: RenderTarget::Window(WindowRef::Entity(window_id)),
+                        ..default()
+                    },
+                    ..default()
+                },
+                PopOutCamera,
+            )).id();
+
+        let ui_root = commands
+            .spawn((
+                NodeBundle{
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        ..default()
+                    },
+                    ..default()
+                },
+                TargetCamera(camera_id),
+            )).id();
+        commands.entity(ui_root).add_child(*add);
+    }
+}
+
+#[derive(Event)]
+pub struct DespawnWindowEvent(pub Entity);
+
+fn despawn_pop_out_window(
+    mut events: EventReader<DespawnWindowEvent>,
+    mut commands: Commands,
+) {
+    for DespawnWindowEvent(window_id) in events.read(){
+        commands.entity(*window_id).despawn_recursive();
+    }
 }
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use PopOutWindow::{plugins::*, prelude::*};
-
-fn main() {
-    App::new()
-        .insert_resource(ClearColor(GOLD))
-        .add_plugins(DefaultPlugins)
-        .run();
-}
